11432c7fd3362c1405fa340c1cdc5c3101fbf2c1
Mike Nieva <miguelnieva@conexionew.com>
2018-05-05 18:41:51 -0500

    Ultimos

---
+++ header.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="app">
+<head>
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<meta charset="UTF-8">
+	<meta name="description" content="VERSE Technology"/>
+	<meta name="google-site-verification" content="eMm5vhErJw5nDnJ0pS3ujhbnRtO_USo7gVHAIw5j4No" />
+	<title>VERSE Technology</title>
+    <meta name="description" content="VERSE Technology. Research, development, implementation and support for the Internet of Things."/>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/css/normalize.css' ?>">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/css/paperkit.css' ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/css/style.css' ?>">
+
+	<link href="<?php echo get_template_directory_uri() . '/favicon.ico';?>" rel="shortcut icon" type="image/png">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+	<!-- Navigation -->
+	<nav class="" role="navigation" style="border-bottom: none; background-color: #08142E">
+		<div class="container-fluid">
+			<div class="container">
+				<a class="navbar-brand" href="https://verse-technology.com/" style="width:100%">
+                    <img alt="" class="logoImg" src="http://localhost:8888/versetechnology-react-wp/wordpress-versetechnology/wp-content/themes/vt/images/Logo.png">
+                </a>
+            </div>
+        </div>
+        
+    
+
+		<div style="display:inline-block;float: right;">
+            <ul class="nav navbar-nav" style="display: inline-block;">
+				<li style="display:inline-block;margin-right: 25px;">
+					<a href="/hardware" style="text-align: center;">Industries</a>
+				</li>
+				<li style="display:inline-block;margin-right: 25px;">
+					<a href="/solutions" style="text-align: center;">Blog</a>
+				</li>
+					<li style="display:inline-block;margin-right: 25px;">
+						<a href="/vt/blog/" style="text-align: center;">Press</a>
+					</li>
+					<li style="display:inline-block;margin-right: 25px;">
+						<a href="/investment" style="text-align: center;">Store</a>
+					</li>
+					<li style="display:inline-block;margin-right: 25px;">
+						<a href="/investment" style="text-align: center;">Careers</a>
+					</li>
+					<li style="display:inline-block;margin-right: 25px;">
+						<a href="/investment" style="text-align: center;">Investment</a>
+					</li>
+					<li style="display: inline-block;margin-right: 25px;"><a href="" style="text-align: center;"><img src="http://localhost:8888/versetechnology-react-wp/wordpress-versetechnology/wp-content/themes/vt/images/united-states.png"></a></li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container -->
+	</nav>
