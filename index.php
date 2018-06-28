c9cc67031a57553a26026069c4e7691bb4dac1e2
Mike Nieva <miguelnieva@conexionew.com>
2018-06-28 10:18:28 -0500

    Prueba

--- index.php
+++ index.php
@@ -1,22 +1,75 @@
 <?php get_header() ?>
-	<section>
-		<div id="inicio" style="background-image: url('<?php echo get_template_directory_uri() . '/images/VT_hero_home.jpg' ?>');">
-				<div class="marketing">
-					<div class="copyprincipal">
-						<h1 style="">Enabling Access to the<br>Industrial Digital World.</h1>	
-					</div>
+
+
+	<section class="completesystems">
+		<div>
+			<h2>
+				Implementing our Complete Systems
+			</h2>
+		</div>
+		<div class="icn">
+			<div class="firstcolumn">
+				<div class="description square1">
 					<figure>
-						<img class="temperaturemetric" src="<?php echo get_template_directory_uri() . '/images/temperature.png'?>">
-						<img class="humiditymetric" src="<?php echo get_template_directory_uri() . '/images/humidity.png'?>" >
+						<img src="<?php echo get_template_directory_uri() . '/images/icn_variable.png'?>">
 					</figure>
-					<figure class="onihero">
-						<img src="<?php echo get_template_directory_uri() . '/images/onifinal.png'?>" alt="">
+					<div class="icndescription">
+						<h4 class="info-title">1. Variable</h4>
+						<p class="description">Measure any type of variable, such as temperature, humidity, conductivity, etc.</p>
+					</div>
+				</div>
+				<div class="description square2">
+					<figure>
+						<img src="<?php echo get_template_directory_uri() . '/images/icn_hardware.png'?>">
+					</figure>
+					<div class="icndescription">
+						<h4 class="info-title">2. Hardware</h4>
+						<p class="description">Install devices anywhere such as fields, pools or enclosed spaces.</p>
+					</div>
+				</div>
+			<div class="description square3">
+				<figure>
+					<img src="<?php echo get_template_directory_uri() . '/images/icn_signal.png'?>">
 				</figure>
+				<div class="icndescription">
+					<h4 class="info-title">3. Connectivity</h4>
+					<p class="description">Connect to the platform at all times thanks to the extensive communication network that reaches remote locations.</p>
 				</div>
 			</div>
+		</div>
+
+		<div class="firstcolumn">
+			<div class="description square4">
+				<figure>
+					<img src="<?php echo get_template_directory_uri() . '/images/icn_monitoring.png'?>">
+				</figure>
+				<div class="icndescription">
+					<h4 class="info-title">4. Monitoring</h4>
+					<p class="description">Access to the necessary information in real time.</p>
+				</div>
+			</div>
+			<div class="description square5">
+				<figure>
+					<img src="<?php echo get_template_directory_uri() . '/images/icn_notifications.png'?>">
+				</figure>
+				<div class="icndescription">
+					<h4 class="info-title">5. Notifications</h4>
+					<p class="description">Receive alerts on any device when a variable is out of range.</p>
+				</div>
+			</div>
+			<div class="description square6">
+				<figure>
+					<img src="<?php echo get_template_directory_uri() . '/images/icn_outcome.png'?>">
+				</figure>
+				<div class="icndescription">
+					<h4 class="info-title">6. Results</h4>
+					<p class="description">Get more benefit from the impact that optimization and automation of your processes will have in making decisions with reliable data.</p>
+				</div>
+			</div>
+
+		</div>
+
 	</section>
-
-
 
 
 <section class="blog-2 section section-gray bloghome">
