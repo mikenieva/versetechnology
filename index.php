3fbd72f70a63210319efbcea41478c275a14bdd6
Mike Nieva <miguelnieva@conexionew.com>
2018-06-28 10:24:53 -0500

    Prueba

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 		<div id="inicio" style="background-image: url('<?php echo get_template_directory_uri() . '/images/VT_hero_home.jpg' ?>');">
 			
 				<div class="marketing">
-					<!--
+					
 					<div class="copyprincipal">
 						<h1 style="">Enabling Access to the<br>Industrial Digital World.</h1>	
 					</div>
-					-->
+					
 					<figure>
 						<img class="temperaturemetric" src="<?php echo get_template_directory_uri() . '/images/temperature.png'?>">
 						<img class="humiditymetric" src="<?php echo get_template_directory_uri() . '/images/humidity.png'?>" >
