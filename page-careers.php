4b1106d157cbed515546111a6a6a837260b1c6a8
Mike Nieva <miguelnieva@conexionew.com>
2018-07-31 01:39:17 -0500

    Careers

--- page-careers.php
+++ page-careers.php
@@ -1,6 +1,34 @@
 <?php get_header() ?>
 
-Hola mundo
+<div class="container">
+    <figure style="text-align:center;">
+        <img style="width: 100%" src="<?php echo get_template_directory_uri() . '/images/vtteam.png'?>" alt="">
+    </figure>
+
+
+    <div class="container">
+        <div style="margin-bottom:50px;">
+        <h2>Careers</h2>
+        <p>En VERSE Technology brindamos soluciones integrales especializadas en tecnología aplicables a cualquier industria, basadas en telemetría, sistemas electrónicos, hardware, y software a la medida. Desarrollamos e implementamos circuitos y sistemas electrónicos avanzados funcionales para agricultura, logística, control y calidad del agua, manufactura, ciudades inteligentes y la industria petrolera. Ofrecemos e integramos los servicios de investigación, desarrollo, implementación y soporte en cualquier lugar de México y América Latina</p>
+        </div>
+        <div class="col-md-4" style="padding:0">
+				<div class="card card-blog" style="box-shadow: 10px 4px 33px -4px rgba(0, 0, 0, 0.15);padding:0;margin-bottom:50px; margin-top: 5px;height: 175px; padding:0">
+					<div class="card-body card-description">
+						<h5 class="card-title titulo">
+								<a href="<?php the_permalink(); ?>">Full Stack Developer</a>
+                        </h5>
+                        <br>
+						<p>
+                            Veracruz, México.
+                            <br/>
+                        </p>
+                        <p>Jornada completa</p>
+                        <p>$15,000 - $25,000 MXN</p>
+					</div>
+				</div>
+			</div>
+        </div>
+</div>
 
 
 <?php get_footer() ?>
